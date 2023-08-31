6019ec5f5a2a1f183974c0be481bc6c0e5cab47a
Jason-Barron07 <jasonbarronyou222@gmail.com>
2023-08-31 14:29:46 +0200

    update

---
+++ streetview/src/components/FooterComp.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+
+        <footer>
+            <div>
+               <p ><i class="bi bi-telephone-fill"> +27 82 604 2972</i></p> 
+               <p><i class="bi bi-envelope-fill"> jasonbarronyou222@gmail</i></p>
+            </div>
+
+            <div>
+                <p><i class="bi bi-github"></i></p>
+                <p><i class="bi bi-instagram"></i></p>
+                <p><i class="bi bi-facebook"></i></p>
+                <p><i class="bi bi-linkedin"></i></p>
+            </div>
+        </footer>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+footer{
+    background-color:#620A15;
+    bottom:0;
+    position: absolute;
+    width:100%;
+    padding:2rem;
+    color:#fff;
+}
+
+p{
+    text-align: center;
+    
+}
+
+
+
+</style>
